4e660ed55c7d56ca55afc6788ec20a9e70db0e28
Sumit Mishra <sumit@macbooks-MacBook-Pro-3.local>
2021-06-21 19:39:48 +0530

    Added util classes

---
+++ app/src/main/java/com/example/weatherlens/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.weatherlens.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+        const val APP_ID = "9b8cb8c7f11c077f8c4e217974d9ee40"
+    }
+}
