7c63b9e73aa4d3fae1334a5f73c7793ff3265bcd
Sumit Mishra <sumit@macbooks-MacBook-Pro-3.local>
2021-06-22 16:38:00 +0530

    Added methods in viewmodel to filter the forecast data for next 4 days; added method to get the day from timestamp

--- app/src/main/java/com/example/weatherlens/activity/mainActivity/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/example/weatherlens/activity/mainActivity/viewmodel/SharedViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.weatherlens.activity.mainActivity.viewmodel
 
+import android.text.format.DateFormat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.weatherlens.activity.mainActivity.model.ForecastData
 import com.example.weatherlens.activity.mainActivity.model.ForecastResponse
 import com.example.weatherlens.activity.mainActivity.model.WeatherResponse
 import com.example.weatherlens.network.WeatherAPI
@@ -12,7 +14,10 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.Response
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
+import kotlin.math.roundToInt
 
 private fun <T> MutableLiveData<T>.asLiveData(): LiveData<T> = this
 const val queryCity = "Bengaluru"
@@ -78,4 +83,46 @@ class SharedViewModel @Inject constructor(
         return Resource.Error(message = response.message())
     }
 
+    fun kelvinToCelsius(temp: Double): Int {
+        return (temp - 273.15).roundToInt()
+    }
+
+    private fun getDayFromTimestamp(timestamp: Int): String =
+        DateFormat.format("EEEE", Date(timestamp.toLong() * 1000)).toString()
+
+    fun prepareForecastList(forecastResponse: ForecastResponse? = null): ArrayList<ForecastData> {
+        val forecastDataList = ArrayList<ForecastData>()
+        val timestampList = mutableListOf<Int>()
+
+        // get current date
+        val date = Date()
+        val formatter = SimpleDateFormat("dd.MM.yyyy")
+        formatter.timeZone = TimeZone.getTimeZone("UTC")
+        val answer: String = formatter.format(date)
+
+        // get timestamp of the current date
+        val baseDateTimestamp =
+            (SimpleDateFormat("dd.MM.yyyy hh:mm:ss").parse("$answer 14:30:00").time / 1000).toInt()
+
+        // create a list of the timestamp of next 4 days at 9:00:00 GMT
+        for (i in 1..4) {
+            timestampList.add(baseDateTimestamp + (i * 86400))
+        }
+
+        // filter the forecast response with data of next 4 days at 9:00:00 GMT
+        forecastResponse?.list?.filter { it -> it.dt in timestampList }?.forEach { it ->
+            forecastDataList.add(
+                ForecastData(
+                    day = getDayFromTimestamp(it.dt),
+                    temperature = kelvinToCelsius(it.main.temp)
+                )
+            )
+        }
+        return forecastDataList
+    }
+
+    fun onRetryBtnClick() {
+        getWeatherData(queryCity)
+        getForecastData(queryCity)
+    }
 }
