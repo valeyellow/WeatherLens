2a2388fc3c515c80b429103ba9abffa49906a1a1
Sumit Mishra <sumit@macbooks-MacBook-Pro-3.local>
2021-06-22 15:52:11 +0530

    Added method in ForecastFragment to receive bundle during instantiation

--- app/src/main/java/com/example/weatherlens/activity/mainActivity/view/ForecastFragment.kt
+++ app/src/main/java/com/example/weatherlens/activity/mainActivity/view/ForecastFragment.kt
@@ -4,11 +4,26 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.weatherlens.R
+import com.example.weatherlens.activity.mainActivity.model.ForecastData
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ForecastFragment: Fragment(R.layout.fragment_forecast) {
+class ForecastFragment : Fragment(R.layout.fragment_forecast) {
+    private lateinit var forecastData: ArrayList<ForecastData>
+
+    companion object {
+        @JvmStatic
+        fun newInstance(data: ArrayList<ForecastData>) = ForecastFragment().apply {
+            arguments = Bundle().apply {
+                putParcelableArrayList("forecastDataList", data)
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        arguments?.getParcelableArrayList<ForecastData>("forecastDataList")?.let {
+            forecastData = it
+        }
     }
 }
